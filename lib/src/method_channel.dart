9672b7a0d90b75b469fe495cba3a3c540542d8d3
George Leonidis <esentakos@yahoo.gr>
2021-05-25 15:39:59 +0300

    🐛 Fixes typecast bug
    'Future<dynamic>' not a subtype of type 'Future<List<dynamic>>'

--- lib/src/method_channel.dart
+++ lib/src/method_channel.dart
@@ -8,8 +8,7 @@ class FlutterDeviceLocaleMethodChannelPlugin
     extends FlutterDeviceLocalePlatform {
   @override
   Future<List<String>> deviceLocales() async {
-    final List<dynamic> result =
-        await (_channel.invokeMethod('deviceLocales') as Future<List<dynamic>>);
+    final List<dynamic> result = await _channel.invokeMethod('deviceLocales');
     return result.cast<String>();
   }
 }
